ffbcde95e9c1c5f6c959a4e62ecd474bca30a54d
Santanu Pattanayak <santanu_pattanayak2003@yahoo.co.in>
2019-01-14 02:26:13 +0530

    Delete MachineTranslationinference.py
    not required

--- Chapter03/MachineTranslationinference.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('/home/santanu/ML_DS_Catalog-/Machine Translation/')
-from MachineTranslation import MachineTranslation
-import pickle
-
-def inference(model_path):
-    with open(model_path,"rb") as f:
-        obj = pickle.load(f)
